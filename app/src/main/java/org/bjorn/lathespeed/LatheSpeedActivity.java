03db4c730cbf31d4a6c574c92fe65cb74ec05c9c
bjorn <bjorn@platypus.(none)>
2016-03-26 17:28:52 -0700

    seek bar control working

--- app/src/main/java/org/bjorn/lathespeed/LatheSpeedActivity.java
+++ app/src/main/java/org/bjorn/lathespeed/LatheSpeedActivity.java
@@ -2,11 +2,16 @@ package org.bjorn.lathespeed;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class LatheSpeedActivity extends AppCompatActivity {
 
+    public static final String TAG = LatheSpeedActivity.class.getSimpleName();
+
     private SeekBar RPMseekBar;
+    private TextView RPMTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +19,24 @@ public class LatheSpeedActivity extends AppCompatActivity {
         setContentView(R.layout.activity_lathe_speed);
 
         // get widget references
+        RPMTextView = (TextView)findViewById(R.id.rpmtextView);
         RPMseekBar = (SeekBar)findViewById(R.id.rpmSeekBar);
+
+        RPMseekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Log.d(TAG, "seek bar value = " + progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
